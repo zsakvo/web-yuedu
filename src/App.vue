611c24c1e9a51b084c6470bf0e06deffa822ba94
zsakvo <gdwqsp@gmail.com>
2019-10-31 15:08:05 +0800

    add toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png" />
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <toolbar />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import toolbar from "./components/Toolbar.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    toolbar
   }
 };
 </script>
@@ -31,6 +22,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
 }
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,11 @@
 import Vue from "vue";
-import { Button } from "element-ui";
+import { Button, Divider, MessageBox, Message } from "element-ui";
 
 Vue.use(Button);
+Vue.use(Divider);
+
+Vue.prototype.$msgbox = MessageBox;
+Vue.prototype.$message = Message;
+Vue.prototype.$alert = MessageBox.alert;
+Vue.prototype.$confirm = MessageBox.confirm;
+Vue.prototype.$prompt = MessageBox.prompt;
