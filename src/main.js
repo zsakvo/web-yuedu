c309cd8ccf9a90080ee2628849d5401fb4953c9c
zsakvo <gdwqsp@gmail.com>
2019-10-31 10:23:55 +0800

    add element-ui

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
+import "./plugins/element.js";
 
 Vue.config.productionTip = false;
 
